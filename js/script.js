40a80403571d9ee299a44283eb07d19f4eb3ae71
umnyash <63366308+umnyash@users.noreply.github.com>
2024-09-04 18:14:05 +0000

    Deploying to gh-pages from @ umnyash/anse@f3466160bf56360f4ab27eee98cbc6c3f2747877 🚀

--- js/script.js
+++ js/script.js
@@ -386,6 +386,40 @@ function initBrandsSlider(brandsElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * catalog-preview-slider.js
+ */
+function initCatalogPreviewSlider(sliderElement) {
+  const paginationElement = sliderElement.querySelector('.slider-pagination');
+  new Swiper(sliderElement, {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    pagination: {
+      el: paginationElement,
+      type: 'progressbar'
+    },
+    breakpoints: {
+      390: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      580: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      790: {
+        slidesPerView: 4,
+        spaceBetween: 10
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * consumers-photos-slider.js
  */
 function initConsumersPhotosSlider(sliderWrapperElement) {
@@ -648,6 +682,7 @@ document.querySelectorAll('.shop__slider-wrapper').forEach(initShopSlider);
 document.querySelectorAll('.simple-filter__slider-wrapper').forEach(initSimpleFilterSlider);
 document.querySelectorAll('.review__slider-wrapper').forEach(initReviewSlider);
 document.querySelectorAll('.brands').forEach(initBrandsSlider);
+document.querySelectorAll('.catalog-preview .products-slider').forEach(initCatalogPreviewSlider);
 let reviews = null;
 let reviewsElement = document.querySelector('.reviews');
 if (reviewsElement) {
