0c988f474daa828bca5bb650c9831693bd80ad09
umnyash <63366308+umnyash@users.noreply.github.com>
2024-09-15 11:33:44 +0000

    Deploying to gh-pages from @ umnyash/anse@776cf0363fb055660322f4f409cdf08a8b8c5a7b 🚀

--- js/script.js
+++ js/script.js
@@ -264,7 +264,7 @@ class Modal {
     this.modalElement.addEventListener('close', () => this.onModalClose());
     this.onOpenerClick = onOpenerClick;
     this.modalElement.addEventListener('click', evt => {
-      if (evt.target === this.modalElement || evt.target.closest('[data-modal-close-button]')) {
+      if (evt.target === this.modalElement || evt.target.closest('[data-modal-close-button]') || !evt.target.closest('.modal__inner')) {
         evt.preventDefault();
         this.close();
       }
@@ -679,27 +679,54 @@ function initProductInfo(productInfoElement) {
  */
 function initProduct(productElement) {
   const sliderElement = productElement.querySelector('.product__slider');
-  new Swiper(sliderElement, {
-    effect: 'creative',
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: ['-20%', 0, -1]
+  const initSlider = () => {
+    const slider = new Swiper(sliderElement, {
+      effect: 'creative',
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ['-20%', 0, -1]
+        },
+        next: {
+          translate: ['100%', 0, 0]
+        }
       },
-      next: {
-        translate: ['100%', 0, 0]
+      speed: 500,
+      pagination: {
+        el: '.product__slider-pagination',
+        bulletClass: 'product__slider-pagination-button',
+        bulletActiveClass: 'product__slider-pagination-button--current',
+        renderBullet: getPaginationButtonCreator(),
+        clickable: true
       }
-    },
-    speed: 500,
-    pagination: {
-      el: '.product__slider-pagination',
-      bulletClass: 'product__slider-pagination-button',
-      bulletActiveClass: 'product__slider-pagination-button--current',
-      renderBullet: getPaginationButtonCreator(),
-      clickable: true
+    });
+  };
+  initProductInfo(productElement.querySelector('.product-info'));
+
+  // Модальное окно с фотографиями и видео
+  const modalElement = document.querySelector('[data-modal="product-gallery"]');
+  const sliderItemElements = Array.from(sliderElement.querySelectorAll('.product__slider-item'));
+  const mediaElements = Array.from(productElement.querySelectorAll('.product__image-wrapper, .product__video-wrapper'));
+  const modalMediaElements = modalElement.querySelectorAll('.modal-gallery__list-item');
+  new SimpleBar(modalElement, {
+    autoHide: false
+  });
+  new Modal(modalElement, {
+    onOpenerClick: evt => {
+      let mediaElementIndex;
+      if (laptopWidthMediaQueryList.matches) {
+        mediaElementIndex = mediaElements.indexOf(evt.currentTarget);
+      } else {
+        mediaElementIndex = sliderItemElements.indexOf(evt.currentTarget);
+      }
+      setTimeout(() => {
+        modalMediaElements[mediaElementIndex].scrollIntoView({
+          behavior: 'smooth'
+        });
+      }, 400);
     }
   });
-  initProductInfo(productElement.querySelector('.product-info'));
+  initSlider();
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -1048,6 +1075,8 @@ document.querySelectorAll('.search, .site-header__search').forEach(initSearch);
 document.querySelectorAll('.folds').forEach(initFolds);
 document.querySelectorAll('.products').forEach(initProducts);
 document.querySelectorAll('.product').forEach(initProduct);
+document.querySelectorAll('[data-modal="offer"]').forEach(modalElement => new Modal(modalElement));
+document.querySelectorAll('[data-modal="size-chart"]').forEach(modalElement => new Modal(modalElement));
 let reviews = null;
 let reviewsElement = document.querySelector('.reviews');
 if (reviewsElement) {
