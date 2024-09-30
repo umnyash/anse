e47b8fa16c13add6cf083e7f0e289f9240d8478b
umnyash <63366308+umnyash@users.noreply.github.com>
2024-09-30 19:04:12 +0000

    Deploying to gh-pages from @ umnyash/anse@ad20aca1f0fca82ecc0cbfbad69861e48b8109ed 🚀

--- js/script.js
+++ js/script.js
@@ -815,53 +815,124 @@ class CheckoutForm extends Form {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
- * alert.js
+ * product.js
  */
 class Product {
   CART_NOTIFICATION_DURATION = 3000;
   constructor(productElement) {
     this.formElement = productElement.querySelector('.product__inner');
     this.cartNotificationElement = document.querySelector('.cart-notification');
-    this.sizeCheckboxElements = Array.from(productElement.querySelectorAll('.size-buttons__control'));
     this.counterElement = productElement.querySelector('.product__cart-counter .counter__control');
     this.counterButtonMinusElement = productElement.querySelector('.product__cart-counter .counter__button--minus');
+    this.colorSectionElement = productElement.querySelector('.product__color-section');
+    this.sizeSectionElement = productElement.querySelector('.product__size-section');
+    this.checkSelectedColor = () => true;
+    this.checkSelectedSize = () => true;
+    this.form = null;
+    this.init();
+  }
+  initColorButtons = () => {
+    const colorOutputElement = this.colorSectionElement.querySelector('.product__color-value');
+    const colorListElement = this.colorSectionElement.querySelector('.product__colors');
+    colorListElement.addEventListener('change', evt => {
+      const colorElement = evt.target.closest('.color');
+      const colorTitle = colorElement.querySelector('.color__title').textContent;
+      colorOutputElement.textContent = colorTitle;
+    });
+  };
+  initColors = () => {
+    if (!this.colorSectionElement) {
+      return;
+    }
+    this.colorModalElement = document.querySelector('[data-modal="color"]');
+    this.colorButtonElements = Array.from(productElement.querySelectorAll('.color__control'));
+    this.colorModalFormElement = this.colorModalElement.querySelector('.modal-color');
+    this.colorModalButtonElements = Array.from(this.colorModalFormElement.querySelectorAll('.color__control'));
+    this.colorModalSubmitButtonElement = this.colorModalFormElement.querySelector('.modal-color__button');
+    this.colorModal = null;
+    this.lockColorModalSubmitButton = () => {
+      this.colorModalSubmitButtonElement.disabled = true;
+      this.colorModalSubmitButtonElement.classList.add('button--disabled');
+    };
+    this.unlockColorModalSubmitButton = () => {
+      this.colorModalSubmitButtonElement.disabled = false;
+      this.colorModalSubmitButtonElement.classList.remove('button--disabled');
+    };
+    this.checkSelectedColorInModalForm = () => {
+      return this.colorModalButtonElements.some(buttonElement => buttonElement.checked);
+    };
+    this.setColorModalSubmitButtonState = () => {
+      const isColorSelected = this.checkSelectedColorInModalForm();
+      if (isColorSelected) {
+        this.unlockColorModalSubmitButton();
+      } else {
+        this.lockColorModalSubmitButton();
+      }
+    };
+    this.onColorModalFormChange = () => {
+      this.setColorModalSubmitButtonState();
+    };
+    this.checkSelectedColor = () => {
+      return this.colorButtonElements.some(buttonElement => buttonElement.checked);
+    };
+    this.openColorModal = () => {
+      this.colorModal.open();
+    };
+    this.closeColorModal = () => {
+      this.colorModal.close();
+    };
+    this.colorModal = new Modal(this.colorModalElement);
+    this.colorModalFormElement.addEventListener('change', this.onColorModalFormChange);
+    this.initColorButtons();
+  };
+  initSizes = () => {
+    if (!this.sizeSectionElement) {
+      return;
+    }
     this.sizeModalElement = document.querySelector('[data-modal="size"]');
+    this.sizeCheckboxElements = Array.from(productElement.querySelectorAll('.size-buttons__control'));
     this.sizeModalFormElement = this.sizeModalElement.querySelector('.modal-size');
     this.sizeModalCheckboxElements = Array.from(this.sizeModalFormElement.querySelectorAll('.size-buttons__control'));
     this.sizeModalSubmitButtonElement = this.sizeModalFormElement.querySelector('.modal-size__button');
     this.sizeModal = null;
-    this.form = null;
-    this.init();
-  }
-  lockSizeModalSubmitButton = () => {
-    this.sizeModalSubmitButtonElement.disabled = true;
-    this.sizeModalSubmitButtonElement.classList.add('button--disabled');
-  };
-  unlockSizeModalSubmitButton = () => {
-    this.sizeModalSubmitButtonElement.disabled = false;
-    this.sizeModalSubmitButtonElement.classList.remove('button--disabled');
-  };
-  checkSelectedSizeInModalForm = () => {
-    return this.sizeModalCheckboxElements.some(checkboxElement => checkboxElement.checked);
-  };
-  setSizeModalSubmitButtonState = () => {
-    const isSizeSelected = this.checkSelectedSizeInModalForm();
-    if (isSizeSelected) {
-      this.unlockSizeModalSubmitButton();
-    } else {
-      this.lockSizeModalSubmitButton();
-    }
+    this.lockSizeModalSubmitButton = () => {
+      this.sizeModalSubmitButtonElement.disabled = true;
+      this.sizeModalSubmitButtonElement.classList.add('button--disabled');
+    };
+    this.unlockSizeModalSubmitButton = () => {
+      this.sizeModalSubmitButtonElement.disabled = false;
+      this.sizeModalSubmitButtonElement.classList.remove('button--disabled');
+    };
+    this.checkSelectedSizeInModalForm = () => {
+      return this.sizeModalCheckboxElements.some(checkboxElement => checkboxElement.checked);
+    };
+    this.setSizeModalSubmitButtonState = () => {
+      const isSizeSelected = this.checkSelectedSizeInModalForm();
+      if (isSizeSelected) {
+        this.unlockSizeModalSubmitButton();
+      } else {
+        this.lockSizeModalSubmitButton();
+      }
+    };
+    this.onSizeModalFormChange = () => {
+      this.setSizeModalSubmitButtonState();
+    };
+    this.checkSelectedSize = () => {
+      return this.sizeCheckboxElements.some(checkboxElement => checkboxElement.checked);
+    };
+    this.openSizeModal = () => {
+      this.sizeModal.open();
+    };
+    this.closeSizeModal = () => {
+      this.sizeModal.close();
+    };
+    this.sizeModal = new Modal(this.sizeModalElement);
+    this.sizeModalFormElement.addEventListener('change', this.onSizeModalFormChange);
   };
   setCounterValue = value => {
     const minValue = +this.counterElement.min;
     this.counterElement.value = value;
     this.counterButtonMinusElement.disabled = +this.counterElement.value <= minValue;
-  };
-  onSizeModalFormChange = () => {
-    this.setSizeModalSubmitButtonState();
-  };
-  checkSelectedSize = () => {
-    return this.sizeCheckboxElements.some(checkboxElement => checkboxElement.checked);
   };
   setAddedToCartView = () => {
     productElement.classList.add('product--in-cart');
@@ -875,26 +946,9 @@ class Product {
       this.cartNotificationElement.close();
     }, this.CART_NOTIFICATION_DURATION);
   };
-  openSizeModal = () => {
-    this.sizeModal.open();
-  };
-  closeSizeModal = () => {
-    this.sizeModal.close();
-  };
-  initColorButtons = () => {
-    const colorSectionElement = this.formElement.querySelector('.product__color-section');
-    const colorOutputElement = colorSectionElement.querySelector('.product__color-value');
-    const colorListElement = colorSectionElement.querySelector('.product__colors');
-    colorListElement.addEventListener('change', evt => {
-      const colorElement = evt.target.closest('.color');
-      const colorTitle = colorElement.querySelector('.color__title').textContent;
-      colorOutputElement.textContent = colorTitle;
-    });
-  };
   init() {
-    this.initColorButtons();
-    this.sizeModal = new Modal(this.sizeModalElement);
-    this.sizeModalFormElement.addEventListener('change', this.onSizeModalFormChange);
+    this.initColors();
+    this.initSizes();
   }
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
@@ -1544,6 +1598,40 @@ function initPageScrollbar(pageInnerElement) {
     }
   });
 }
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * product-card-colors.js
+ */
+function initProductCardColors(listElement) {
+  const calculateFittingItems = () => {
+    for (const item of listElement.children) {
+      const colorListElement = item.querySelector('.product-card__colors-list');
+      if (!colorListElement) {
+        continue;
+      }
+      const counterElement = item.querySelector('.product-card__colors-counter');
+      counterElement.classList.add('product-card__colors-counter--hidden');
+      let listWidth = colorListElement.offsetWidth;
+      const itemWidth = parseInt(getComputedStyle(colorListElement.querySelector('.product-card__colors-item')).width);
+      const gap = parseInt(getComputedStyle(colorListElement).columnGap);
+      let maxItems = Math.floor((listWidth + gap) / (itemWidth + gap));
+      let quantityOverflowItems = colorListElement.children.length - maxItems;
+      if (!quantityOverflowItems) {
+        continue;
+      }
+      counterElement.textContent = `+${quantityOverflowItems}`;
+      counterElement.classList.remove('product-card__colors-counter--hidden');
+      listWidth = colorListElement.offsetWidth;
+      maxItems = Math.floor((listWidth + gap) / (itemWidth + gap));
+      quantityOverflowItems = colorListElement.children.length - maxItems;
+      counterElement.textContent = `+${quantityOverflowItems}`;
+    }
+  };
+  calculateFittingItems();
+  window.addEventListener('resize', throttleAndDebounce(calculateFittingItems, 500));
+}
+
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
@@ -2216,6 +2304,7 @@ document.querySelectorAll('.catalog__sorting').forEach(initCatalogSorting);
 document.querySelectorAll('.catalog').forEach(catalogElement => {
   initCatalogFilter(catalogElement, simpleBar.getScrollElement());
 });
+document.querySelectorAll('.products-list__items, products-slider__list').forEach(initProductCardColors);
 document.querySelectorAll(`
   .birth-date-modal-form .select__control,
   .modal-form--region .select__control,
