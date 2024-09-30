5c0146b146331c180784b4e004187faa5fd82761
umnyash <63366308+umnyash@users.noreply.github.com>
2024-09-30 06:29:14 +0000

    Deploying to gh-pages from @ umnyash/anse@31311a9d2f9d1690848256cc2fd6e2c12fde96b0 🚀

--- js/script.js
+++ js/script.js
@@ -595,7 +595,7 @@ class BirthDateModalForm extends Modal {
     super(modalElement);
     this.formElement = modalElement.querySelector('.birth-date-modal-form');
     this.dayFieldElement = this.formElement.querySelector('.birth-date-modal-form__item-day input');
-    this.monthFieldElement = this.formElement.querySelector('.birth-date-modal-form__month-select');
+    this.monthFieldElement = this.formElement.querySelector('.birth-date-modal-form__item-month .select__control');
     this.yearFieldElement = this.formElement.querySelector('.birth-date-modal-form__item-year input');
     this.submitButtonElement = this.formElement.querySelector('.birth-date-modal-form__submit-button');
     this.backButtonElement = this.formElement.querySelector('.birth-date-modal-form__back-button');
@@ -695,6 +695,121 @@ class BirthDateModalForm extends Modal {
     this.formElement.addEventListener('input', this.onFormInput);
     this.formElement.addEventListener('submit', this.onFormSubmit);
     this.backButtonElement.addEventListener('click', this.onBackButtonClick);
+  };
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ *  checkout-form.js
+ */
+
+class CheckoutForm extends Form {
+  deliveryCompanyStateClasses = {
+    sdek: 'checkout-form--delivery-company_sdek',
+    post: 'checkout-form--delivery-company_post',
+    selfPickup: 'checkout-form--delivery-company_self-pickup'
+  };
+  deliveryMethodStateClasses = {
+    point: 'checkout-form--delivery-method_point',
+    courier: 'checkout-form--delivery-method_courier'
+  };
+  constructor(formElement) {
+    super(formElement);
+    this.formElement = formElement;
+    this.regionFieldElement = this.formElement.querySelector('[data-name="region"]');
+    this.deliveryCompanyButtonsWrapper = this.formElement.querySelector('.checkout-form__delivery-company');
+    this.phoneFieldElement = this.formElement.querySelector('.checkout-form__phone-field .text-field__control');
+    this.regionModalElement = document.querySelector('[data-modal="region-form"]');
+    this.regionFormElement = this.regionModalElement.querySelector('.modal-form');
+    this.regionFormCountryFieldElement = this.regionFormElement.querySelector('.modal-form__country-select select');
+    this.regionFormCountryOptionElements = Array.from(this.regionFormElement.querySelectorAll('.modal-form__country-select option'));
+    this.regionFormCityFieldElement = this.regionFormElement.querySelector('.modal-form__city-field .text-field__control');
+    this.regionFormSubmitButtonElement = this.regionFormElement.querySelector('.modal-form__submit-button');
+    this.regionModal = new Modal(this.regionModalElement);
+    this.addressModalElement = document.querySelector('[data-modal="address-form"]');
+    this.addressFormElement = this.addressModalElement.querySelector('.modal-form');
+    this.addressFormCountryFieldElement = this.addressFormElement.querySelector('.modal-form__country-select select');
+    this.addressFormCountrySelect = initSelect(this.addressFormCountryFieldElement);
+    this.addressFormCountryOptionElements = Array.from(this.addressFormElement.querySelectorAll('.modal-form__country-select option'));
+    this.addressFormCityFieldElement = this.addressFormElement.querySelector('.modal-form__city-field .text-field__control');
+    this.addressModal = new Modal(this.addressModalElement, {
+      onOpenerClick: () => {
+        // console.log(this.regionFormCountryFieldElement.value)
+        this.addressFormCountrySelect.setChoiceByValue(this.regionFormCountryFieldElement.value);
+        this.addressFormCityFieldElement.value = this.regionFormCityFieldElement.value;
+      }
+    });
+    this.init();
+  }
+  getSelectedOption = (selectElement, optionElements) => optionElements.find(optionElement => optionElement.value === selectElement.value);
+  onRegionFormSubmitButtonClick = () => {
+    const countryText = this.getSelectedOption(this.regionFormCountryFieldElement, this.regionFormCountryOptionElements).textContent;
+    const cityText = this.regionFormCityFieldElement.value;
+    const regionText = `${countryText}, ${cityText}`;
+    this.regionFieldElement.value = regionText;
+    this.deliveryCompanyButtonsWrapper.classList.remove('checkout-form__delivery-company--hidden');
+    this.regionModal.close();
+  };
+  setDeliveryCompanySdekState = () => {
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.post);
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.selfPickup);
+    this.formElement.classList.add(this.deliveryCompanyStateClasses.sdek);
+  };
+  setDeliveryCompanyPostState = () => {
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.sdek);
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.selfPickup);
+    this.formElement.classList.add(this.deliveryCompanyStateClasses.post);
+  };
+  setDeliveryCompanySelfPickupState = () => {
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.sdek);
+    this.formElement.classList.remove(this.deliveryCompanyStateClasses.post);
+    this.formElement.classList.add(this.deliveryCompanyStateClasses.selfPickup);
+  };
+  setDeliveryMethodCourierState = () => {
+    this.formElement.classList.remove(this.deliveryMethodStateClasses.point);
+    this.formElement.classList.add(this.deliveryMethodStateClasses.courier);
+  };
+  setDeliveryMethodPointState = () => {
+    this.formElement.classList.add(this.deliveryMethodStateClasses.point);
+    this.formElement.classList.remove(this.deliveryMethodStateClasses.courier);
+  };
+  onFormChange = evt => {
+    const deliveryCompanyButtonElement = evt.target.closest('.checkout-form__delivery-company');
+    const deliveryMethodButtonElement = evt.target.closest('.checkout-form__delivery-methods');
+    if (deliveryCompanyButtonElement) {
+      const buttonTitle = evt.target.dataset.title;
+      switch (buttonTitle) {
+        case 'sdek':
+          this.setDeliveryCompanySdekState();
+          break;
+        case 'post':
+          this.setDeliveryCompanyPostState();
+          break;
+        case 'self-pickup':
+          this.setDeliveryCompanySelfPickupState();
+          break;
+      }
+      return;
+    }
+    if (deliveryMethodButtonElement) {
+      const buttonTitle = evt.target.dataset.title;
+      switch (buttonTitle) {
+        case 'courier':
+          this.setDeliveryMethodCourierState();
+          break;
+        case 'pick-up-point':
+          this.setDeliveryMethodPointState();
+          break;
+      }
+      return;
+    }
+  };
+  init = () => {
+    new IMask(this.phoneFieldElement, {
+      mask: '[+]00000000000000000'
+    });
+    this.regionFormSubmitButtonElement.addEventListener('click', this.onRegionFormSubmitButtonClick);
+    this.formElement.addEventListener('change', this.onFormChange);
   };
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
@@ -1746,7 +1861,7 @@ function initSearch(searchWrapperElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 function initSelect(selectElement) {
-  new Choices(selectElement, {
+  return new Choices(selectElement, {
     // eslint-disable-line
     searchEnabled: false,
     itemSelectText: '',
@@ -2101,7 +2216,11 @@ document.querySelectorAll('.catalog__sorting').forEach(initCatalogSorting);
 document.querySelectorAll('.catalog').forEach(catalogElement => {
   initCatalogFilter(catalogElement, simpleBar.getScrollElement());
 });
-document.querySelectorAll('.select').forEach(initSelect);
+document.querySelectorAll(`
+  .birth-date-modal-form .select__control,
+  .modal-form--region .select__control,
+  .profile-form .select__control
+`).forEach(initSelect);
 document.querySelectorAll('.checkout-order__products').forEach(initCheckoutProductsSlider);
 let subscriptionForm = null;
 let subscriptionFormElement = document.querySelector('.subscription-form');
@@ -2122,6 +2241,11 @@ let modalEntry = null;
 const modalEntryElement = document.querySelector('[data-modal="entry"]');
 if (modalEntryElement) {
   modalEntry = new ModalEntry(modalEntryElement);
+}
+let checkoutForm = null;
+const checkoutFormElement = document.querySelector('.checkout-form');
+if (checkoutFormElement) {
+  checkoutForm = new CheckoutForm(checkoutFormElement);
 }
 let profileForm = null;
 const profileFormElement = document.querySelector('.profile-form');
