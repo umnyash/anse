47415dfb43e1d86ee26c2d073ad20d95c592f1d2
umnyash <63366308+umnyash@users.noreply.github.com>
2024-10-23 07:27:38 +0000

    Deploying to gh-pages from @ umnyash/anse@50b10a0c2118037147a4d60e815772282fe86e96 🚀

--- js/script.js
+++ js/script.js
@@ -2484,6 +2484,41 @@ function initSkeletons() {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * text-field.js
+ */
+function initTextField(fieldElement) {
+  const dataListWrapperElement = fieldElement.querySelector('.text-field__data-list-wrapper');
+  if (!dataListWrapperElement) {
+    return;
+  }
+  const dataListElement = dataListWrapperElement.querySelector('.text-field__data-list');
+  const updateBlur = () => {
+    const isScrollableDown = dataListElement.scrollHeight > dataListElement.scrollTop + dataListElement.clientHeight;
+    const isScrollableUp = dataListElement.scrollTop > 0;
+    dataListWrapperElement.classList.toggle('text-field__data-list-wrapper--blur-bottom', isScrollableDown);
+    dataListWrapperElement.classList.toggle('text-field__data-list-wrapper--blur-top', isScrollableUp);
+  };
+  const fieldObserver = new MutationObserver(() => {
+    if (fieldElement.classList.contains('text-field--list-open')) {
+      updateBlur();
+    }
+  });
+  fieldObserver.observe(fieldElement, {
+    attributes: true,
+    attributeFilter: ['class']
+  });
+  const listObserver = new MutationObserver(() => {
+    updateBlur();
+  });
+  listObserver.observe(dataListElement, {
+    childList: true,
+    subtree: true
+  });
+  dataListElement.addEventListener('scroll', updateBlur);
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * main.js
  */
 const laptopWidthMediaQueryList = window.matchMedia(LAPTOP_WIDTH_MEDIA_QUERY);
@@ -2572,4 +2607,5 @@ if (birthDateModalFormElement && birthDateInfoModal) {
 }
 initReviewsIntroPhotos();
 initConsumersPhotos();
+document.querySelectorAll('.text-field').forEach(initTextField);
 /* * * * * * * * * * * * * * * * * * * * * * * */
