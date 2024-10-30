564619e85c9570b07329ad568cf0ba0be10a0657
umnyash <63366308+umnyash@users.noreply.github.com>
2024-10-30 09:55:34 +0000

    Deploying to gh-pages from @ umnyash/anse@5c00a7fd682437abcd1c45fd2325ab02608bda3e 🚀

--- js/script.js
+++ js/script.js
@@ -1176,6 +1176,10 @@ class Reviews {
     this.reviewsElement = reviewsElement;
     this.listElement = reviewsElement.querySelector('.reviews-list');
     this.reviewsWrapperElement = this.listElement.querySelector('.reviews-list__items');
+    this.reviewPhotosModalElement = document.querySelector('[data-modal="review-photos"]');
+    this.reviewPhotosModal = null;
+    this.reviewPhotosModalSliderElement = null;
+    this.reviewPhotosModalSlider = null;
     this.init();
   }
   updateReviewsTextWrappersList = () => {
@@ -1199,6 +1203,9 @@ class Reviews {
   };
   initReviewSlider = sliderWrapperElement => {
     const sliderElement = sliderWrapperElement.querySelector('.review__slider');
+    if (!sliderElement) {
+      return;
+    }
     new Swiper(sliderElement, {
       slidesPerView: 'auto',
       spaceBetween: 10,
@@ -1250,8 +1257,60 @@ class Reviews {
     this.updateReviewsTextWrappersList();
   };
   onWindowResize = debounce(this.setReviewsTextWrappersMode, 500);
+  createReviewPhotosModalSlider = photos => {
+    const sliderString = `
+      <div class="gallery-slider modal__gallery-slider">
+        <div class="gallery-slider__slider swiper">
+          <ul class="gallery-slider__list swiper-wrapper">
+            ${photos.map(photo => `
+              <li class="gallery-slider__item swiper-slide">
+                <picture class="gallery-slider__image-wrapper media media--cover media--position_top skeleton">
+                  <img class="gallery-slider__image media__image" src="${photo}" width="622" height="786" alt="" loading="lazy">
+                </picture>
+              </li>
+            `).join('')}
+          </ul>
+        </div>
+        <div class="slider-arrows gallery-slider__arrows slider-arrows--big">
+          <ul class="slider-arrows__list">
+            <li class="slider-arrows__item">
+              <button class="slider-arrows__button slider-arrows__button--prev" type="button">
+                <span class="visually-hidden">Предыдущий</span>
+              </button>
+            </li>
+            <li class="slider-arrows__item">
+              <button class="slider-arrows__button slider-arrows__button--next" type="button">
+                <span class="visually-hidden">Следующий</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    `;
+    return createElementByString(sliderString);
+  };
+  updateReviewModalSlider = photos => {
+    this.reviewPhotosModalSlider?.destroy();
+    this.reviewPhotosModalSliderElement?.remove();
+    this.reviewPhotosModalSliderElement = this.createReviewPhotosModalSlider(photos);
+    this.reviewPhotosModalElement.querySelector('.modal__inner').append(this.reviewPhotosModalSliderElement);
+    initSkeletons(this.reviewPhotosModalSliderElement);
+    this.reviewPhotosModalSlider = initGallerySlider(this.reviewPhotosModalSliderElement);
+  };
   onListClick = evt => {
+    const reviewPhotoSlideElement = evt.target.closest('.review__slider-item');
     const toggleButtonElement = evt.target.closest('.review__toggle-button');
+    if (reviewPhotoSlideElement) {
+      const photosSlideElements = Array.from(reviewPhotoSlideElement.parentElement.querySelectorAll('.review__slider-item'));
+      const reviewPhotos = photosSlideElements.map(slideElement => slideElement.querySelector('img').src);
+      if (!reviewPhotos.length) {
+        return;
+      }
+      const photosNumber = photosSlideElements.indexOf(reviewPhotoSlideElement);
+      this.updateReviewModalSlider(reviewPhotos);
+      this.reviewPhotosModal.open();
+      this.reviewPhotosModalSlider.slideTo(photosNumber, 0);
+    }
     if (toggleButtonElement) {
       const textWrapperElement = toggleButtonElement.closest('.review__text-wrapper');
       textWrapperElement.classList.toggle('review__text-wrapper--clipped');
@@ -1262,6 +1321,7 @@ class Reviews {
     this.setReviewsTextWrappersMode();
     this.reviewsWrapperElement.querySelectorAll('.review__slider-wrapper').forEach(this.initReviewSlider);
     window.addEventListener('resize', this.onWindowResize);
+    this.reviewPhotosModal = new Modal(this.reviewPhotosModalElement);
     this.listElement.addEventListener('click', this.onListClick);
   }
 }
